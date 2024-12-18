3096ff50fcc5921685100f2bae15fffd22756ca9
lsuchel <luke.suchel@live.com>
2024-12-18 01:15:56 -0800

    12/17

--- libwyag.py
+++ libwyag.py
@@ -656,3 +656,30 @@ This function is aware of:
 
     return candidates
 
+def object_find(repo, name, fmt=None, follow=True):
+    sha = object_resolve(repo, name)
+
+    if not sha:
+        raise Exception("No such reference {0}.".format(name))
+    
+    sha = sha[0]
+
+    if not fmt:
+        return sha
+    
+    while True:
+        obj = object_read(repo, sha)
+
+        if obj.fmt == fmt:
+            return sha
+        
+        if not follow:
+            return None
+        
+        if obj.fmt == b'tag':
+            sha = obj.kvlm[b'object'].decode("ascii")
+        elif obj.fmt == b'commit' and fmt == b'tree':
+            sha = obj.kvlm[b'tree'].decode("ascii")
+        else:
+            return None
+        
