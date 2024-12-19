b4aa615e56b7b6cc45e0505d7c074cbbbc6d9abf
lsuchel <luke.suchel@live.com>
2024-12-19 15:30:02 -0800

    12/19

--- libwyag.py
+++ libwyag.py
@@ -710,3 +710,30 @@ def cmd_rev_parse(args):
 
     print (object_find(repo, args.name, fmt, follow=True))
 
+class GitIndexEntry (object):
+    def __init__(self, ctime=None, mtime=None, dev=None, ino=None, mode_type=None, mode_perms=None, uid=None, gid=None, fsize=None, sha=None, flag_assume_valid=None, flag_stage=None, name=None):
+        self.ctime = ctime
+        self.mtime = mtime
+        self.dev = dev
+        self.ino = ino
+        self.mode_type = mode_type
+        self.mode_perms = mode_perms
+        self.uid = uid
+        self.gid = gid
+        self.fsize = fsize
+        self.sha = sha
+        self.flag_assume_valid = flag_assume_valid
+        self.flag_stage = flag_stage
+        self.name = name
+
+class GitIndex (object):
+    version = None
+    entries = []
+
+    def __init__(self, version=2, entries=None):
+        if not entries:
+            entries = list()
+
+        self.version = version
+        self.entries = entries
+
